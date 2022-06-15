14435009a0899bdd52edbb8254323c8722bb083b
EndriEm <endrimyshkeri@.com>
2022-06-16 01:43:38 +0200

    first commit

---
+++ index1.js
@@ -0,0 +1,8 @@
+let list = [
+  ["Milk", 1.2],
+  ["Cocoa", 2.0],
+  ["Salad", 2.0],
+  ["Carrots", 2.0],
+  ["Tomatoes", 2.5],
+  ["Ready meals", 5.0],
+];
---
+++ index2.js
@@ -0,0 +1,2 @@
+let items = ["Milk", "Cocoa", "Salad", "Carrots", "Tomatoes", "Ready meals"];
+let prices = [1.2, 2.0, 2.0, 2.0, 2.5, 5.0];
