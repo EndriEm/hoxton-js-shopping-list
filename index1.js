beff1b72cb76679b87030673c20d8ea330918600
EndriEm <endrimyshkeri@.com>
2022-06-17 01:52:27 +0200

    changes

--- index1.js
+++ index1.js
@@ -6,3 +6,27 @@ let list = [
   ["Tomatoes", 2.5],
   ["Ready meals", 5.0],
 ];
+
+let userWantsToAddItem = confirm("Add an item?");
+
+while (userWantsToAddItem) {
+  let itemName = String(
+    prompt("What is the name of the item you want to add?")
+  );
+  let itemPrice = Number(
+    prompt("What is the price of the item you want to add?")
+  );
+  list.push([itemName, itemPrice]);
+
+  userWantsToAddItem = confirm("Add another item?");
+}
+
+let total = 0;
+for (let item of list) {
+  let name = item[0];
+  let price = item[1];
+  total += price;
+  console.log(`${name}: £${price.toFixed(2)}`);
+}
+
+console.log(`Total: £${total.toFixed(2)}`);
--- index2.js
+++
@@ -1,2 +0,0 @@
-let items = ["Milk", "Cocoa", "Salad", "Carrots", "Tomatoes", "Ready meals"];
-let prices = [1.2, 2.0, 2.0, 2.0, 2.5, 5.0];
